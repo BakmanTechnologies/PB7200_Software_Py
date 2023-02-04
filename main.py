479a5e9014188a472cfe98b87189af79d96abf9c
RFranco9 <rickfranco9@gmail.com>
2023-02-04 10:55:26 -0800

    removed pm support

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ def main():
     serial_data_manipulation = SerialDataManipulation(cal_data)
     serial_data_manipulation.display_system_info()
     # serial_data_manipulation.dwell(200, 100, 100)
-    serial_data_manipulation.scan(200, 600, 1, 100)
-    # serial_data_manipulation.dwell_pm(500, 100, 10, 2.5)
-    # serial_data_manipulation.scan_pm(1300, 1500, 0.5, 100, 2.5)
+    # serial_data_manipulation.scan(200, 600, 1, 100)
     serial_data_manipulation.close_port()
 
 
